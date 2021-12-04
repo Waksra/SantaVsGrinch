34acfa9d5fa4b459e0975738c20bb4ec5f8fc3e5
Wiktor Ravndal <w.ravndal@gmail.com>
2021-12-04 19:18:59 +0100

    Player dash and prefab

--- SantaVsGrinch/Assets/Scripts/Player/Movement.cs
+++ SantaVsGrinch/Assets/Scripts/Player/Movement.cs
@@ -8,16 +8,31 @@ namespace Player
     {
         [SerializeField, Range(0, 50)] private float maxSpeed = 10f;
         [SerializeField, Range(0, 50)] private float maxAcceleration = 10f;
-        [SerializeField] private float maxAngle;
+        [SerializeField, Range(0, 90)] private float maxAngle;
+        
+        [Space(10)]
+        
+        [SerializeField, Range(0, 100)] private float dashMaxSpeed = 20f;
+        [SerializeField, Range(0, 500)] private float dashAcceleration = 200f;
+        [SerializeField, Range(0, 5)] private float dashCooldown = 0.75f;
+        [SerializeField, Range(0, 2)] private float dashRequestBuffer = 0.5f;
+        [SerializeField] private AnimationCurve dashCurve;
 
         private Rigidbody body;
         private Vector2 inputVector;
 
         private Vector3 velocity;
 
-        private float minDot;
+        private float minGroundDot;
         private int contactCount;
         private Vector3 groundNormal;
+
+        private bool dashRequested;
+        private bool isDashing;
+        private Vector3 dashDirection;
+        private float dashRequestEnd;
+        private float dashTime;
+        private float nextDashTime;
 
         private bool IsGround => contactCount > 0;
 
@@ -26,21 +41,36 @@ namespace Player
             inputVector = context.ReadValue<Vector2>();
         }
 
+        public void RequestDash(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+            {
+                dashRequested = true;
+                dashRequestEnd = dashRequestBuffer + Time.time;
+            }
+        }
+
         private void OnValidate()
         {
-            minDot = Mathf.Cos(maxAngle * Mathf.Deg2Rad);
+            minGroundDot = Mathf.Cos(maxAngle * Mathf.Deg2Rad);
         }
 
         private void Awake()
         {
-            body = GetComponent<Rigidbody>();
             OnValidate();
+            body = GetComponent<Rigidbody>();
+            
+            if(dashCurve.length == 0)
+                Debug.LogError(gameObject.name + " has no dash curve set.");
         }
 
         private void FixedUpdate()
         {
             UpdateState();
-            AdjustVelocity();
+            if (isDashing)
+                Dash();
+            else
+                AdjustVelocity();
 
             body.velocity = velocity;
             ClearState();
@@ -62,6 +92,14 @@ namespace Player
             velocity += xAxis * adjustment.x + zAxis * adjustment.y;
         }
 
+        private void Dash()
+        {
+            float curveEvaluation = dashCurve.Evaluate(dashTime);
+            Vector3 desiredVelocity = dashDirection * Mathf.LerpUnclamped(maxSpeed, dashMaxSpeed, curveEvaluation);
+
+            velocity = Vector3.MoveTowards(velocity, desiredVelocity, dashAcceleration * Time.deltaTime);
+        }
+
         private void UpdateState()
         {
             velocity = body.velocity;
@@ -74,6 +112,32 @@ namespace Player
             else
             {
                 groundNormal = Vector3.up;
+            }
+
+            if (dashCurve.length == 0)
+                return;
+
+            if (isDashing)
+            {
+                dashTime += Time.deltaTime;
+            
+                if (dashTime < dashCurve[dashCurve.length - 1].time)
+                    return;
+
+                isDashing = false;
+                nextDashTime = Time.time + dashCooldown;
+            }
+            
+            if (dashRequested && !isDashing && Time.time >= nextDashTime)
+            {
+                isDashing = true;
+                dashRequested = false;
+                dashDirection = ProjectDirectionGround(new Vector3(inputVector.x, 0, inputVector.y));
+                dashTime = 0f;
+            }
+            else if (dashRequested && dashRequestEnd >= Time.time)
+            {
+                dashRequested = false;
             }
         }
 
@@ -90,7 +154,7 @@ namespace Player
                 Vector3 normal = collision.GetContact(i).normal;
 
                 float upDot = Vector3.Dot(Vector3.up, normal);
-                if(upDot < minDot)
+                if(upDot < minGroundDot)
                     continue;
 
                 groundNormal += normal;
