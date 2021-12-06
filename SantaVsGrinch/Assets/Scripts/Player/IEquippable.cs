aee5530844923b14931a655659ca89fca5208d03
Wiktor Ravndal <w.ravndal@gmail.com>
2021-12-06 16:40:33 +0100

    Gun

---
+++ SantaVsGrinch/Assets/Scripts/Player/IEquippable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace Player
+{
+    public interface IEquippable
+    {
+        void Equip(EquipmentHolder owner);
+        
+        void OnActivate();
+
+        void OnDeactivate();
+    }
+}
