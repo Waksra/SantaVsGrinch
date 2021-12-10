a04c7414f14914845af09348da1dea51dc8f21a0
Wiktor Ravndal <w.ravndal@gmail.com>
2021-12-10 16:13:36 +0100

    On hit event and more knockback events

--- SantaVsGrinch/Assets/Scripts/Gameplay/Knockbacker.cs
+++ SantaVsGrinch/Assets/Scripts/Gameplay/Knockbacker.cs
@@ -33,6 +33,36 @@ namespace Gameplay
             Vector3 knockbackDir = new Vector3(velocity.x, 0f, velocity.z).normalized; 
             knockbackable.Knockback(knockbackDir * knockback);
         }
+        
+        public void KnockbackAlongVelocity(Collision collision)
+        {
+            if(!collision.gameObject.TryGetComponent(out Knockbackable knockbackable))
+                return;
+
+            Vector3 velocity = body.velocity;
+            Vector3 knockbackDir = new Vector3(velocity.x, 0f, velocity.z).normalized; 
+            knockbackable.Knockback(knockbackDir * knockback);
+        }
+
+        public void KnockbackAway(Collider other)
+        {
+            if(!other.TryGetComponent(out Knockbackable knockbackable))
+                return;
+
+            Vector3 direction = other.transform.position - body.position;
+            Vector3 knockbackDir = new Vector3(direction.x, 0f, direction.z).normalized; 
+            knockbackable.Knockback(knockbackDir * knockback);
+        }
+        
+        public void KnockbackAway(Collision collision)
+        {
+            if(!collision.gameObject.TryGetComponent(out Knockbackable knockbackable))
+                return;
+
+            Vector3 direction = collision.transform.position - body.position;
+            Vector3 knockbackDir = new Vector3(direction.x, 0f, direction.z).normalized; 
+            knockbackable.Knockback(knockbackDir * knockback);
+        }
 
         public void KnockbackRadial()
         {
---
+++ SantaVsGrinch/Assets/Scripts/Gameplay/OnHitEvent.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Gameplay
+{
+    public class OnHitEvent : MonoBehaviour
+    {
+        private UnityEvent<Collider> onTrigger;
+        private UnityEvent<Collision> OnCollision;
+        
+        private void OnTriggerEnter(Collider other)
+        {
+            onTrigger?.Invoke(other);
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            OnCollision?.Invoke(collision);
+        }
+    }
+}
